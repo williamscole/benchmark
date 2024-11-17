15dba4e144a1e9a3aae0923f9c4bcd132d145263
Cole Williams <47306731+williamscole@users.noreply.github.com>
2024-11-17 13:47:28 -0500

    Update run_ibis.sh

--- run_ibis.sh
+++ run_ibis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+# run_ibis.sh
 ibis=${1}
 bfile=${2}
 threads=${3}
+BASE_PATH=${4:-/}  # Optional base path, defaults to /
 
-cd /data/tmp
-
+cd "${BASE_PATH}/data/tmp"
 $ibis -b $bfile -t $threads -printCoef -ibd2
