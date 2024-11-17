b9a18db27b40b01411aef941f01744393aa9f6a7
Cole Williams <47306731+williamscole@users.noreply.github.com>
2024-11-17 13:47:42 -0500

    Update run_crest.sh

--- run_crest.sh
+++ run_crest.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -e
-set -x
-
+# run_crest.sh
 crest_dir=${1}
 glen=${2}
 bim=${3}
+BASE_PATH=${4:-/}  # Optional base path, defaults to /
 
-cd /data/tmp
+cd "${BASE_PATH}/data/tmp"
 
 echo "Individual1     Individual2     Kinship_Coefficient     IBD2_Fraction   Segment_Count   Degree" > ibis_2nd.coef
 
@@ -15,7 +14,7 @@ cat ibis.coef | awk '{ if ($6 < 4 && $6 > 0) print }' >> ibis_2nd.coef || {
 }
 
 echo "Running crest_ratio"
-${crest_dir}/crest_ratio -i ibis.seg -r ibis_2nd.coef -o ratio --ibd2 0.17 --pc --kinship_lw 0.05 --kinship_up 0.25  || {
+${crest_dir}/crest_ratio -i ibis.seg -r ibis_2nd.coef -o ratio --ibd2 0.17 --pc --kinship_lw 0.05 --kinship_up 0.25 || {
     exit 1
 }
 
@@ -25,6 +24,6 @@ ${crest_dir}/CREST_relationships.py -i ratio.csv --total_len $glen || {
 }
 
 echo "Running CREST_sex_inference.py"
-${crest_dir}/CREST_sex_inference.py -i ibis.seg -m /opt/benchmark/refined_mf.simmap -b ${bim} -o crest_output.tsv -k ibis_2nd.coef || {
+${crest_dir}/CREST_sex_inference.py -i ibis.seg -m "${BASE_PATH}/opt/benchmark/refined_mf.simmap" -b ${bim} -o crest_output.tsv -k ibis_2nd.coef || {
     exit 1
 }
